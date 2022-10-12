d14aba931e0668341da5a3bcc14b7956f907ff23
Wandile121 <109789985+Wandile121@users.noreply.github.com>
2022-10-12 09:46:19 +0200

    Add files via upload

---
+++ tic tac toe/index.js
@@ -0,0 +1,23 @@
+window.addEventListener('DOMContentLoaded', () => {
+    const tiles = Array.from(document.querySelectorAll('.tile'));
+    const playerDisplay = document.querySelector('.display-player');
+    const resetButton = document.querySelector('#reset');
+    const announcer = document.querySelector('.announcer');
+
+    let board = ['', '', '', '', '', '', '', '', ''];
+    let currentPlayer = 'X';
+    let isGameActive = true;
+
+    const PLAYERX_WON = 'PLAYERX_WON';
+    const PLAYERO_WON = 'PLAYERO_WON';
+    const TIE = 'TIE';
+
+    /*
+        Indexes within the board
+
+
+
+
+
+    resetButton.addEventListener('click', resetBoard);
+});
